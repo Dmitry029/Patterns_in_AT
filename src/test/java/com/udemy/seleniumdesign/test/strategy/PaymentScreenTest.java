c10bc8229b3ebcda6fd6f5cc51fd5c239633dee3
Dmitry029 <kdp.029029@gmail.com>
2020-12-15 13:17:14 +0300

    Pattern strategy is ready.

---
+++ src/test/java/com/udemy/seleniumdesign/test/strategy/PaymentScreenTest.java
@@ -0,0 +1,57 @@
+package com.udemy.seleniumdesign.test.strategy;
+
+;
+import com.udemy.seleniumdesign.strategy.CreditCard;
+import com.udemy.seleniumdesign.strategy.NetBanking;
+import com.udemy.seleniumdesign.strategy.PaymentOption;
+import com.udemy.seleniumdesign.strategy.PaymentScreen;
+import com.udemy.seleniumdesign.test.BaseTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.collections.Maps;
+
+import java.util.Map;
+
+public class PaymentScreenTest extends BaseTest {
+
+    private PaymentScreen paymentScreen;
+
+    @BeforeTest
+    public void setPaymentScreen() {
+        this.paymentScreen = new PaymentScreen(this.driver);
+    }
+
+    @Test(dataProvider = "getData")
+    public void paymentTest(PaymentOption paymentOption, Map<String, String> paymentDetails) {
+        this.paymentScreen.goTo();
+        this.paymentScreen.getUserInformation().enterDetails("vinoth", "selvaraj", "udemy@gmail.com");
+        this.paymentScreen.setPaymentOption(paymentOption);
+        this.paymentScreen.pay(paymentDetails);
+
+        String orderNumber = this.paymentScreen.getOrder().placeOrder();
+
+        System.out.println(
+                "Order Number : " + orderNumber
+        );
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        Map<String, String> cc = Maps.newHashMap();
+        cc.put("CC", "1231231231");
+        cc.put("year", "2023");
+        cc.put("cvv", "123");
+
+        Map<String, String> nb = Maps.newHashMap();
+        nb.put("bank", "WELLS FARGO");
+        nb.put("account", "myaccount123");
+        nb.put("pin", "999");
+
+        return new Object[][]{
+                {new CreditCard(), cc},
+                {new NetBanking(), nb}
+        };
+
+    }
+}
